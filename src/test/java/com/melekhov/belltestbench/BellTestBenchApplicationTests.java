08d9df3def55f6cfbecfd3d3fb7abc10dbb2058c
krutoy <cool.melekhov@gmail.com>
2025-08-19 17:57:32 +0400

    Initial commit

---
+++ src/test/java/com/melekhov/belltestbench/BellTestBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melekhov.belltestbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellTestBenchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
